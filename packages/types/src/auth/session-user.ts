b01a782faab54a1fcf6452d4c671709b5ebd3e56
Eric Campbell <ericcampbell59@gmail.com>
2023-06-25 12:51:50 -0500

    update SessionUser

--- packages/types/src/auth/session-user.ts
+++ packages/types/src/auth/session-user.ts
@@ -1,16 +1,32 @@
 export interface SessionUser {
-    id: string;
-    sessionId: string;
-    firstName: string;
-    lastName: string;
-    username: string;
-    email: string;
-    emailVerified: boolean;
-    phone: string;
-    phoneVerified: boolean;
-    image: string;
-    timezone: string;
-    locale: string;
-    orgId: string;
-    orgRole: string;
+    sub: string;
+    iss: string;
+    iat: number;
+    exp: number;
+    aud: string;
+    claims?: {
+        sessionId: string;
+        firstName: string;
+        lastName: string;
+        username: string;
+        image: string;
+        email: string;
+        phone: string | undefined;
+        orgId: string | undefined;
+    };
+
+    // id: string;
+    // sessionId: string;
+    // firstName: string;
+    // lastName: string;
+    // username: string;
+    // email: string;
+    // emailVerified: boolean;
+    // phone: string;
+    // phoneVerified: boolean;
+    // image: string;
+    // timezone: string;
+    // locale: string;
+    // orgId: string;
+    // orgRole: string;
 }
