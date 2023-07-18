bc075380d5012ba42d2d3fd6b8ea421d3596fbc1
Eric Campbell <ericcampbell59@gmail.com>
2023-07-18 15:31:31 -0500

    asdf

--- packages/auth-react/src/components/public/sign-in/routes/success.tsx
+++ packages/auth-react/src/components/public/sign-in/routes/success.tsx
@@ -1,7 +1,15 @@
 import { AuthComponentType } from '@protoxyz/themes';
 import { CardWrapper } from '../../../custom-ui/card-wrapper';
-import { Card } from '../../../ui/card';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '../../../ui/card';
 import { useProtocolAuthAppearance } from '../../../../contexts/protocol-context';
+import { BrandLogo, BrandLogoWrapper } from '../../../custom-ui/brand-logo';
+import { Spinner } from '../../../ui/spinner';
 
 export function SignInSuccessRoute() {
   const component: AuthComponentType = 'signIn';
@@ -12,7 +20,24 @@ export function SignInSuccessRoute() {
       component={component}
       className={appearance?.elements?.cardWrapper}
     >
-      <Card className={appearance?.elements?.card}>SignInSuccessRoute</Card>
+      <Card className={appearance?.elements?.card}>
+        <CardHeader className={appearance?.elements?.cardHeader}>
+          <BrandLogoWrapper>
+            <BrandLogo component={component} />
+          </BrandLogoWrapper>
+          <CardTitle className={appearance?.elements?.cardHeaderTitle}>
+            Sign In Successful
+          </CardTitle>
+          <CardDescription
+            className={appearance?.elements?.cardHeaderDescription}
+          >
+            You'll be redirected shortly.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className={appearance?.elements?.cardContent}>
+          <Spinner size="lg" />
+        </CardContent>
+      </Card>
     </CardWrapper>
   );
 }
