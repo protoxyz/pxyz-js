17cb9b2902a05169c4773e4efc7bfcd835ece83c
Eric Campbell <ericcampbell59@gmail.com>
2023-07-26 10:56:07 -0500

    get token in middleware

--- packages/auth-nextjs/src/server.ts
+++ packages/auth-nextjs/src/server.ts
@@ -2,6 +2,9 @@ export { ProtocolAuthProvider } from './server/protocol-provider.js';
 export { getUser } from './server/getUser.js';
 export { getAuth } from './server/getAuth.js';
 export { getToken } from './server/getToken.js';
-export { getMiddlewareAuth } from './server/getMiddlewareAuth.js';
+export {
+  getMiddlewareAuth,
+  getMiddlewareToken,
+} from './server/getMiddlewareAuth.js';
 export * from './server/paths.js';
 export { authMiddleware } from './server/authMiddleware.js';
--- packages/auth-nextjs/src/server/getMiddlewareAuth.ts
+++ packages/auth-nextjs/src/server/getMiddlewareAuth.ts
@@ -3,6 +3,12 @@ import { getBearerToken, getCookieToken, getSecretKey } from './util';
 import { verifyJWT } from './jwt';
 import { SessionUser } from '@protoxyz/types';
 
+export async function getMiddlewareToken({ headers }: { headers: Headers }) {
+  return (
+    (await getCookieToken({ headers })) || (await getBearerToken({ headers }))
+  );
+}
+
 export async function getMiddlewareAuth({
   req,
   key,
@@ -12,8 +18,7 @@ export async function getMiddlewareAuth({
 }) {
   const headers = req.headers;
 
-  const token =
-    (await getCookieToken({ headers })) || (await getBearerToken({ headers }));
+  const token = await getMiddlewareToken({ headers });
 
   if (!token) return null;
 
