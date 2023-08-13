45d947faab93b7306957d5dbc5bf9afbbe0b5e58
Eric Campbell <ericcampbell59@gmail.com>
2023-08-13 08:12:14 -0500

    use appearance

--- apps/example-auth-nextjs/src/app/layout.tsx
+++ apps/example-auth-nextjs/src/app/layout.tsx
@@ -37,7 +37,7 @@ export default async function RootLayout({
     <html suppressHydrationWarning>
       <body className={inter.className}>
         {/* @ts-ignore */}
-        <ProtocolAuthProvider>
+        <ProtocolAuthProvider appearance={appearance}>
           <Header />
           {children}
         </ProtocolAuthProvider>
